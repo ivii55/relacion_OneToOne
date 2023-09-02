c627158a397ffe219aa1fccfbe3b576e1cae77bb
ivii55 <39770277+ivii55@users.noreply.github.com>
2023-09-02 13:07:31 -0300

    Add files via upload

---
+++ ejer02/src/main/java/com/utn/ejer02/entidades/Domicilio.java
@@ -0,0 +1,30 @@
+package com.utn.ejer02.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Domicilio implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String calle;
+
+    private int numero;
+
+
+}
---
+++ ejer02/src/main/java/com/utn/ejer02/entidades/Persona.java
@@ -0,0 +1,37 @@
+package com.utn.ejer02.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private int edad;
+
+    // RELACION ONE TO ONE UNIDIRECCIONAL
+    // El Cascadeo propaga las operaciones y orphanRemoval asegura que se elimine la relacionada
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;
+
+
+
+
+}
